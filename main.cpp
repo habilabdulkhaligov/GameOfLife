95699cb0acef2a1f12f5a07db0eb5dc53dc19a10
Habil <c.imran2001@gmail.com>
2022-06-18 23:48:10 +0400

    Main modified

--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <unistd.h>
+#include "Table.hpp"
+#include "Exceptions.hpp"
+
+#define WIDTH  50
+#define HEIGHT 25
+#define FPS 2
+#define DENSITY 1
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Table *table;
+
+    try {
+        table = new Table(WIDTH, HEIGHT);
+    } catch (MemoryException &ex) {
+        std::cout << ex.what() << std::endl;
+        return -1;
+    }
+
+    try {
+        table->fill(DENSITY);
+    } catch (InvalidArgumentException &ex) {
+        std::cout << ex.what() << std::endl;
+        delete table;
+        return -1;
+    }
+
+    std::cout << *table << std::endl;
+
+    useconds_t timeDelay = 1000000 / FPS;
+
+    for (int i = 0; i < 1000; i++) {
+        table->update();
+        usleep(timeDelay);
+        std::cout << *table << std::endl;
+    }
+
+    delete table;
     return 0;
 }
