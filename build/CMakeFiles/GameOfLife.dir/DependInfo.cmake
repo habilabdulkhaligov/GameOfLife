215b9e95706fbfef55ef5b5dbc8821efef124906
Habil <c.imran2001@gmail.com>
2022-06-19 23:51:08 +0400

    CMake build configuration added

---
+++ build/CMakeFiles/GameOfLife.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/talehtaghiyev/Downloads/GameOfLife-master/Exceptions.cpp" "CMakeFiles/GameOfLife.dir/Exceptions.cpp.o" "gcc" "CMakeFiles/GameOfLife.dir/Exceptions.cpp.o.d"
+  "/Users/talehtaghiyev/Downloads/GameOfLife-master/Table.cpp" "CMakeFiles/GameOfLife.dir/Table.cpp.o" "gcc" "CMakeFiles/GameOfLife.dir/Table.cpp.o.d"
+  "/Users/talehtaghiyev/Downloads/GameOfLife-master/main.cpp" "CMakeFiles/GameOfLife.dir/main.cpp.o" "gcc" "CMakeFiles/GameOfLife.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/GameOfLife.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GameOfLife.dir/Exceptions.cpp.o"
+  "CMakeFiles/GameOfLife.dir/Exceptions.cpp.o.d"
+  "CMakeFiles/GameOfLife.dir/Table.cpp.o"
+  "CMakeFiles/GameOfLife.dir/Table.cpp.o.d"
+  "CMakeFiles/GameOfLife.dir/main.cpp.o"
+  "CMakeFiles/GameOfLife.dir/main.cpp.o.d"
+  "GameOfLife"
+  "GameOfLife.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GameOfLife.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
